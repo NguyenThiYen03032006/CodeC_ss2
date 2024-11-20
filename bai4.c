c3da2a2f6b2d079537010c18693d915b06edec11
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2024-11-20 13:37:37 +0700

    Add files via upload

---
+++ bai4.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("nhap do dai canh hinh vuong ");
+    scanf("%d", &a);
+    int P= a*4;
+    printf("chu vi hinh vuong la : %d\n", P);
+    int S= a*a;
+    printf("dien tich hinh vuong la : %d\n", S);
+    return 0;
+}
