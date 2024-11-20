9518e585b5eda4eba936c320c5dc02d48722ad8b
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2024-11-20 13:36:38 +0700

    Add files via upload

---
+++ bai1.c
@@ -0,0 +1,5 @@
+// khai bao va khoi tao cac bien voi cac kieu du lieu co ban trong C
+int tuoi = 18; // bien tuoi co kdl so nguyen voi gia tri 18
+char toan = 'T';// bien toan co kdl ky tu voi gia tri A
+float pi = 3.14;// bien pi co kdl so thuc voi gia tri 3.14
+double e = 2.71828182846;// bien e co kdl so thuc voi gia tri 2.71828182846
