5edac5238c2a496d9c4de288b2edea0c3e1158a0
NguyenThiYen03032006 <nguyenthiyen03032006@gmail.com>
2024-11-20 13:36:59 +0700

    Add files via upload

---
+++ bai2.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a, b;
+    printf("nhap so thu nhat ");
+    scanf("%d", &a);
+    printf("nhap so thu hai ");
+    scanf("%d", &b);
+    int tong = a +b;
+    printf("tong 2 so la : %d\n", tong);
+    return 0;
+    }
